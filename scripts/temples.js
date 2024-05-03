2957071b6eac6871a6a714958bbb1562be6acbff
Emily McGovern <mcg21013@byui.edu>
2024-05-03 17:04:54 +0200

    temples update

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,11 @@ year.innerHTML = today.getFullYear();
 let modif = new Date(document.lastModified).toLocaleString('en-us');
 const lastModified = document.querySelector("#lastModified");
 lastModified.innerHTML = `Last Modified: <span>${modif}</span>`;
+
+const hamButton = document.querySelector('#hamburger-x');
+const navigation = document.querySelector('#top-nav');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
